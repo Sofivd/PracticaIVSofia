bfe1fd59e7c34b7503f844dca6ad58a48e17861f
Sofivd <svd08062001svd@gmail.com>
2023-12-11 17:04:35 +0000

    ARREGLADO ARBOLES

--- Assets/Scripts/BotonCrear.cs
+++ Assets/Scripts/BotonCrear.cs
@@ -9,28 +9,66 @@ public class BotonCrear : MonoBehaviour
     public GameObject Interfaz;
     public GameObject arbolVerde;
     public GameObject arbolMarron;
+    public GameObject[] Creaciones = new GameObject[4];
+    static int BichoSeleccionado = 0;
+    static bool Creacion = false;
 
+    private void Update()
+    {
+        if(Creacion == true)
+        {
+            Arbol1();
+        }
+    }
     public void CrearPulsado()
     {
-        MenuCrear.gameObject.SetActive(true);
-        Interfaz.gameObject.SetActive(false);
+        if(Creacion == false)
+        {
+            MenuCrear.gameObject.SetActive(true);
+            Interfaz.gameObject.SetActive(false);
+            Creacion = true;
+        }
+        else
+        {
+            Creacion = false;
+            MenuCrear.gameObject.SetActive(false);
+            Interfaz.gameObject.SetActive(true);
+        }
+        
     }
     public void Arbol1()
     {
-        arbolVerde.SetActive(true);
 
         if (Input.GetMouseButtonDown(0))
         {
+           
             Ray rayoCamara = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit crear;
-            if (Physics.Raycast(rayoCamara, out crear))
+            int mascara = LayerMask.GetMask("Suelo");
+            if (Physics.Raycast(rayoCamara, out crear,Mathf.Infinity ,mascara))
             {
-               // GameObject.Instantiate(arbolVerde, crear.point);
+                Debug.Log("Inside");
+                // qUATERNION.IDENTITY -> (0,0,0)
+                GameObject.Instantiate(Creaciones[BichoSeleccionado], crear.point, Creaciones[BichoSeleccionado].transform.rotation);
             }
         }
     }
-    public void Arbol2()
+    public void ArbolVerde()
     {
-        arbolMarron.SetActive(true);
+        BichoSeleccionado = 0;
+    }
+    public void ArbolMarron()
+    {
+        BichoSeleccionado = 1;
+    }
+
+    public void Granero()
+    {
+        BichoSeleccionado = 2;
+    }
+
+    public void Casita()
+    {
+        BichoSeleccionado = 3;
     }
 }
